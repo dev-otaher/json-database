b8ed3884f924d3e73b4cfc8fc6f0aebf09055797
Omar Ahmad <dev.omar.taher@gmail.com>
2023-06-01 02:03:13 +0300

    Stage 2/6

--- JSON Database (Java)/task/src/client/Main.java
+++ JSON Database (Java)/task/src/client/Main.java
@@ -1,7 +1,25 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
+        try (Socket socket = new Socket("127.0.0.1", 34522);
+             DataInputStream input = new DataInputStream(socket.getInputStream());
+             DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+            System.out.println("Client started!");
 
+            String toBeSent = "Give me a record # 12";
+            output.writeUTF(toBeSent);
+            System.out.println("Sent: " + toBeSent);
+
+            String received = input.readUTF();
+            System.out.println("Received: " + received);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- JSON Database (Java)/task/src/server/Main.java
+++ JSON Database (Java)/task/src/server/Main.java
@@ -1,33 +1,28 @@
 package server;
 
-import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) {
-        Database database = new Database();
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        while (true) {
-            input = scanner.nextLine();
-            if (input.equals("exit")) {
-                break;
-            }
-            String[] tokens = input.split(" ", 3);
-            String command = tokens[0];
-            int index = Integer.parseInt(tokens[1]) - 1;
-            switch (command) {
-                case "set":
-                    String text = tokens[2];
-                    System.out.println(database.set(index, text) ? "OK" : "ERROR");
-                    break;
-                case "get":
-                    System.out.println(database.get(index));
-                    break;
-                case "delete":
-                    System.out.println(database.delete(index) ? "OK" : "ERROR");
-                    break;
-            }
+        try (ServerSocket server = new ServerSocket(34522)) {
+            System.out.println("Server started!");
+            Socket socket = server.accept();
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            String received = input.readUTF();
+            System.out.println("Received: " + received);
+
+            String toBeSent = "A record # 12 was sent!";
+            output.writeUTF(toBeSent);
+            System.out.println("Sent: " + toBeSent);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
